b7f33e3e5a36540ce8d2ce2f7ee569b754f1d032
Breno Aquino <brenoaquino7phr@gmail.com>
2022-02-08 22:47:51 -0300

    create unit tests cases for repositories

---
+++ Data/Tests/DataTests/Repositories/CategoriesRepositoryTests.swift
@@ -0,0 +1,14 @@
+//
+//  CategoriesRepositoryTests.swift
+//  
+//
+//  Created by Breno Aquino on 08/02/22.
+//
+
+import XCTest
+
+class CategoriesRepositoryTests: XCTestCase {
+    
+    func testSuccessToDomainType() { XCTFail("Empty test") }
+    func testErrorToDomainType() { XCTFail("Empty test") }
+}
