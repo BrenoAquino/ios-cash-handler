e8de40ac47d6b00037e2c373841d6b8af001217e
Breno Aquino <brenoaquino7phr@gmail.com>
2022-02-27 15:20:45 -0300

    fix baseURL

--- Data/Sources/Data/DataSources/Remote/Operations/OperationsAPIs.swift
+++ Data/Sources/Data/DataSources/Remote/Operations/OperationsAPIs.swift
@@ -13,7 +13,7 @@ enum OperationsAPIs {
 
 extension OperationsAPIs: APIs {
     var baseURL: String {
-        return "https://tqcabp1qk03.execute-api.us-east-1.amazonaws.com/dev/cash-management"
+        return "https://tqcbp1qk03.execute-api.us-east-1.amazonaws.com/dev/cash-management"
     }
     
     var path: String {
