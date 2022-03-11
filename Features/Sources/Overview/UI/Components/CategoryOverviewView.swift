e3cb5812bc3da8359a4448702318219f6100604b
Breno Aquino <brenoaquino7phr@gmail.com>
2022-03-11 14:30:02 -0300

    fix some issues

--- Features/Sources/Overview/UI/Components/CategoryOverviewView.swift
+++ Features/Sources/Overview/UI/Components/CategoryOverviewView.swift
@@ -13,29 +13,7 @@ struct CategoryOverviewView: View {
     @State var categoryOverview: CategoryOverviewUI
     
     var body: some View {
-        HStack(alignment: .center) {
-            element(title: "Total Entrada", value: overviewMont.income)
-            element(title: "Total Saída", value: overviewMont.expense)
-            element(title: "Acréscimo", value: overviewMont.refer)
-        }
-        .frame(maxWidth: .infinity)
-        .padding(DSSpace.smallL.rawValue)
-        .background(DSColor.secondBackground.rawValue)
-        .cornerRadius(DSCornerRadius.normal.rawValue)
-        .shadow(style: .medium)
-    }
-    
-    private func element(title: String, value: String) -> some View {
-        VStack(alignment: .leading) {
-            Text(title)
-                .foregroundColor(DSColor.primaryText.rawValue)
-                .font(DSFont.subheadlineLarge.rawValue)
-            
-            Text(value)
-                .foregroundColor(DSColor.primaryText.rawValue)
-                .font(DSFont.title2.rawValue)
-        }
-        .frame(maxWidth: .infinity)
+        EmptyView()
     }
 }
 
