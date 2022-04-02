7eeb2bd25832e28f7fd6094b2062f07dca46e09a
Breno Aquino <brenoaquino7phr@gmail.com>
2022-04-02 13:38:31 -0300

    fix ectension

--- Common/Sources/Common/Extensions/Encodable+asDictionary.swift
+++ Common/Sources/Common/Extensions/Encodable+asDictionary.swift
@@ -10,7 +10,7 @@ import Foundation
 public extension Encodable {
     func asDictionary() throws -> [String: Any] {
         let data = try JSONEncoder().encode(self)
-        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: String] else {
+        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
             throw NSError()
         }
         return dictionary
